3e359eeafac03e4d9fa73fb6e8007f473ff1d073
niuguorui-li <127124049+niuguorui-li@users.noreply.github.com>
2023-07-19 14:16:37 +0000

    第一篇文章测试提交 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/19/hello-world/","2023/07/19/第一篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
